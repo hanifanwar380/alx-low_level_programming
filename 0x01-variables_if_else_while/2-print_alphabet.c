4600dfbd2e6ff5c52d14cb4dc8dc048c77ce1460
hanifanwar380 <hanifanwar380@gmail.com>
2022-04-11 15:47:42 -0700

    program that prints the alphabet in lowercase, followed by a new line.

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,23 +1,45 @@
 /*
- * File: 2-print_alphabet.c
- * Auth: Hanif Anwar
- */
+ *
+ *  * File: 2-print_alphabet.c
+ *
+ *   * Auth: William
+ *
+ *    */
+
+
 
 #include <stdio.h>
 
+
+
 /**
- * main - Prints the alphabet in lowercase.
  *
- * Return: Always 0.
- */
+ *  * main - Prints the alphabet in lowercase.
+ *
+ *   *
+ *
+ *    * Return: Always 0.
+ *
+ *     */
+
 int main(void)
+
 {
-  char letter;
 
-  for (letter = 'a'; letter <= 'z'; letter++)
-    putchar(letter);
+		char letter;
 
-  putchar('\n');
 
-  return (0);
+
+			for (letter = 'a'; letter <= 'z'; letter++)
+
+						putchar(letter);
+
+
+
+				putchar('\n');
+
+
+
+					return (0);
+
 }
