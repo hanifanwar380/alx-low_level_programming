e451a71e78eb25e0692f4b3c5b593721a1c3c9c8
hanifanwar380 <hanifanwar380@gmail.com>
2022-04-13 22:34:24 +0300

    program that prints _putchar, followed by a new line.

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,14 +3,14 @@
 /**
  *  * main - prints the string "Holberton" from a character array.
  *   * Return: 0 on success.
- *   */
+ */
 
 int main(void)
 {
-	char n[9] = "_putchar";
-	printf("%s", n);
+char n[9] = "_putchar";
+printf("%s", n);
 
-	printf("\n");
-	return (0);
+printf("\n");
+return (0);
 
 }
