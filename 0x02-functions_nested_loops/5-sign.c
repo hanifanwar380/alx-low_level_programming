53efb9dd87305bc8e86d4889bbd448814d146c6f
hanifanwar380 <hanifanwar380@gmail.com>
2022-04-13 17:50:51 +0300

    function that prints the sign of a number.

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -6,10 +6,11 @@
  *    * Description: Tohis function prints the sign of the number
  *     * Return: 1 if number is positive, o if number is 0, or
  *      * -1 if number is negative
- *       */
+ */
 int print_sign(int n)
 {
 	int value;
+
 	if (n > 0)
 	{
 		value = 1;
