092fe157426dd6b8e9fbe671b8812a2dc379f76f
hanifanwar380 <hanifanwar380@gmail.com>
2022-04-20 00:21:04 +0300

    function that draws a diagonal line on the terminal.

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- *  * print_diagonal -  checks for checks for a digit (0 through 9).
+ * print_diagonal -  checks for checks for a digit (0 through 9).
  * @n: n -  Variable
  *   
  * Return: Always 0.
