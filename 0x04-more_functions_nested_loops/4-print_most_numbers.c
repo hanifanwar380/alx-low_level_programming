e69ebea396d4eda59915b9484fef7c15edaf6c2d
hanifanwar380 <hanifanwar380@gmail.com>
2022-04-19 18:27:41 +0300

    function that prints the numbers, from 0 to 9, followed by a new line, skips 2 and 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- *  * print_most_numbers -  checks for checks for a digit (0 through 9).
- *   *
- *    * Return: Always 0.
- *     */
+ * print_most_numbers -  checks for checks for a digit (0 through 9).
+ *
+ * Return: Always 0.
+ */
 void print_most_numbers(void)
 }
 int a;
