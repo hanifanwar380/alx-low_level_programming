2070fd50bf7811ef3229bec277a7914c570e9431
hanifanwar380 <hanifanwar380@gmail.com>
2022-04-19 18:36:12 +0300

    function that prints the numbers, from 0 to 9, followed by a new line, except 2 and 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,10 +1,12 @@
 #include "main.h"
 
 /**
- * print_most_numbers -  prints except 2 and 4..
+ * print_most_numbers - prints except 2 and 4..
  *
  * Return: Always 0.
+ *
  */
+
 void print_most_numbers(void)
 }
 int a;
