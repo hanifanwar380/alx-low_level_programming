f263cae7346ff4533a3b51b3687d2dd0258a5e1a
hanifanwar380 <hanifanwar380@gmail.com>
2022-04-24 15:20:17 +0300

    function that compares two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,7 +4,6 @@
  * _strcmp - Compares pointers to two strings.
  * @s1: A pointer to the first string to be compared.
  * @s2: A pointer to the second string to be compared.
- *
  * Return: If str1 < str2, the negative difference of the first unmatched
  * characters.
  * If str1 == str2, 0.
