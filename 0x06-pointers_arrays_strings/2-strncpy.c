1b61f8915c00cce3cc9a791c1d4d1424eab05909
hanifanwar380 <hanifanwar380@gmail.com>
2022-04-24 12:06:18 +0300

    function that copies a string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strncpy - writes a function that copies a string
+ * @dest: pointer to dest string
+ * @src: pointer to src string
+ * @n: number of bytes from src
+ * Return: the pointer to dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+int i;
+
+i = 0;
+while (i < n && *(src + i))
+{
+*(dest + i) = *(src + i);
+i++;
+}
+while (i < n)
+{
+*(dest + i) = '\0';
+i++;
+}
+return (dest);
+}
