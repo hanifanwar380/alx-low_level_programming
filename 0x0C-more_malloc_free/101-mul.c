751b5cecda051ef0f068de0a1dc21a977971a424
hanifanwar380 <hanifanwar380@gmail.com>
2022-05-10 21:33:11 +0300

    more mallocs

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 /**
 * _isNum - check if is a number
 *@num: string to check
@@ -9,14 +10,14 @@
 */
 int _isNum(char *num)
 {
-	int i;
+int i;
 
-	for (i = 0; num[i] != '\0'; i++)
-	{
-		if (num[i] < '0' || num[i] > '9')
-			return (0);
-	}
-	return (1);
+for (i = 0; num[i] != '\0'; i++)
+{
+if (num[i] < '0' || num[i] > '9')
+return (0);
+}
+return (1);
 }
 
 /**
@@ -28,11 +29,11 @@ int _isNum(char *num)
 */
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i;
+unsigned int i;
 
-	for (i = 0; i < n; i++)
-		s[i] = b;
-	return (s);
+for (i = 0; i < n; i++)
+s[i] = b;
+return (s);
 }
 
 /**
@@ -42,12 +43,12 @@ char *_memset(char *s, char b, unsigned int n)
 */
 int _strlen(char *s)
 {
-	int len;
+int len;
 
-	len = 0;
-	while (*(s + len) != '\0')
-		len++;
-	return (len);
+len = 0;
+while (*(s + len) != '\0')
+len++;
+return (len);
 }
 
 /**
@@ -58,41 +59,41 @@ int _strlen(char *s)
 */
 int main(int argc, char *argv[])
 {
-	int length, c, prod, i, j, l1, l2;
-	int *res;
+int length, c, prod, i, j, l1, l2;
+int *res;
 
-	if ((argc != 3 || !(_isNum(argv[1]))) || !(_isNum(argv[2])))
-		puts("Error"), exit(98);
-	l1 = _strlen(argv[1]), l2 = _strlen(argv[2]);
-	length = l1 + l2;
-	res = calloc(length, sizeof(int *));
-	if (res == NULL)
-		puts("Error"), exit(98);
-	for (i = l2 - 1; i > -1; i--)
-	{
-		c = 0;
-		for (j = l1; j > -1; j--)
-		{
-			prod = (argv[2][i] - '0') * (argv[1][j] - '0');
-			c = (prod / 10);
-			res[(i + j) + 1] += (prod % 10);
-			if (res[(i + j) + 1] > 9)
-			{
-				res[i + j] += res[(i + j) + 1] / 10;
-				res[(i + j) + 1] = res[(i + j) + 1] % 10;
-			}
-			res[(i + j) + 1] += c;
-		}
-	}
+if ((argc != 3 || !(_isNum(argv[1]))) || !(_isNum(argv[2])))
+puts("Error"), exit(98);
+l1 = _strlen(argv[1]), l2 = _strlen(argv[2]);
+length = l1 + l2;
+res = calloc(length, sizeof(int *));
+if (res == NULL)
+puts("Error"), exit(98);
+for (i = l2 - 1; i > -1; i--)
+{
+c = 0;
+for (j = l1; j > -1; j--)
+{
+prod = (argv[2][i] - '0') * (argv[1][j] - '0');
+c = (prod / 10);
+res[(i + j) + 1] += (prod % 10);
+if (res[(i + j) + 1] > 9)
+{
+res[i + j] += res[(i + j) + 1] / 10;
+res[(i + j) + 1] = res[(i + j) + 1] % 10;
+}
+res[(i + j) + 1] += c;
+}
+}
 
-	if (res[0] == 0)
-		i = 1;
-	else
-		i = 0;
-	for (; i < length; i++)
-		printf("%d", res[i]);
+if (res[0] == 0)
+i = 1;
+else
+i = 0;
+for (; i < length; i++)
+printf("%d", res[i]);
 
-	printf("\n");
-	free(res);
-	return (0);
+printf("\n");
+free(res);
+return (0);
 }
