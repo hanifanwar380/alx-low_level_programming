26da9b803430c8bd91e5ad6c16ff30171482f186
hanifanwar380 <hanifanwar380@gmail.com>
2022-05-27 22:32:00 +0300

    git push

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,6 +1,6 @@
 /*
  * File: 2-add_nodeint.c
- * Auth: Brennan D Baraban
+ * Auth: Hanif Miyanji
  */
 
 #include "lists.h"
@@ -15,6 +15,7 @@
  * Return: If the function fails - NULL.
  *         Otherwise - the address of the new element.
  */
+
 listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *new;
