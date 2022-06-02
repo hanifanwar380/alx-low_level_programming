2a56316c4206a758a817950b2cbbbca00a556bf9
hanifanwar380 <hanifanwar380@gmail.com>
2022-06-02 21:30:28 +0300

    function that creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
   * create_file - ...
   * @filename: ...
@@ -7,6 +6,7 @@
   *
   * Return: ...
   */
+
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
@@ -31,6 +31,7 @@ int create_file(const char *filename, char *text_content)
   *
   * Return: String length
   */
+
 int _strlen(char *s)
 {
 	int c = 0;
